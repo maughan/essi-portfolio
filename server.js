109e50f78d4ca7478df2660bd6c1f4a17198c4ec
RMaughan93 <rhys.maughan93@gmail.com>
2020-04-09 23:07:11 +0100

    lord please

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from 'express';
-
+import path from 'path';
 const app = express();
 const port = 3000;
 
-app.use(express.static('build'))
+
+app.use(express.static('build'));
+
+app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")))
 
 app.listen(port, err => {
   if(err) throw err;
